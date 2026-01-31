051db810ccd4d669b68400ae6cee5e41e7cd741b
artb10 <arthurcn99@gmail.com>
2026-01-31 20:24:33 -0300

    Criando services para as duas entidades

--- empresa/src/main/java/com/arthur/empresa/services/DepartamentoService.java
+++ empresa/src/main/java/com/arthur/empresa/services/DepartamentoService.java
@@ -1,10 +1,13 @@
 package com.arthur.empresa.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.arthur.empresa.entities.Departamento;
+import com.arthur.empresa.exceptions.DadosInvalidosException;
 import com.arthur.empresa.exceptions.NaoEncontradoException;
 import com.arthur.empresa.repositories.DepartamentoRepository;
 
@@ -16,8 +19,48 @@ public class DepartamentoService {
 	
 	public Departamento procurarPorId(Long Id){
 		return repository.findById(Id).
-				orElseThrow(() -> new NaoEncontradoException("ID Inválido! Usuário não encontrado."));
+				orElseThrow(() -> new NaoEncontradoException("ID Inválido! Departamento não encontrado."));
 		
 	}
 	
+	public List<Departamento> procurarTodos(){
+		List<Departamento> listaDpto = repository.findAll();
+		return listaDpto;
+		
+	}
+	
+	public String criarDepartamento(Departamento dpto) {
+		
+		if (dpto.getEmail() == null || dpto.getEmail().isBlank()) {
+			throw new DadosInvalidosException("Email é obrigatório.");
+		}
+		if(dpto.getNome() == null || dpto.getNome().isBlank()) {
+			throw new DadosInvalidosException("Nome é obrigatório.");
+		}
+		
+		repository.save(dpto);
+		return "Departamento salvo no sistema";
+		
+	}
+	
+	public String editarDepartamento(Long id, Departamento dpto) {
+		Departamento response = procurarPorId(id);
+		
+		if(dpto.getEmail() != null) {
+			response.setEmail(dpto.getEmail());
+		}
+		
+		if(dpto.getNome() != null) {
+			response.setNome(dpto.getNome());
+		}
+		
+		repository.save(response);
+		return "Departamento atualizado com sucesso";
+	}
+	
+	public String deletarDepartamento(Long id) {
+		Departamento response = procurarPorId(id);
+		repository.delete(response);
+		return "Departamento deletado.";
+	}
 }
---
+++ empresa/src/main/java/com/arthur/empresa/services/FuncionarioService.java
@@ -0,0 +1,67 @@
+package com.arthur.empresa.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arthur.empresa.entities.Funcionario;
+import com.arthur.empresa.exceptions.DadosInvalidosException;
+import com.arthur.empresa.exceptions.NaoEncontradoException;
+import com.arthur.empresa.repositories.FuncionarioRepository;
+
+@Service
+public class FuncionarioService {
+	
+	@Autowired
+	private FuncionarioRepository repository;
+	
+	public Funcionario procurarPorId(Long Id){
+		return repository.findById(Id).
+				orElseThrow(() -> new NaoEncontradoException("ID Inválido! Funcionario não encontrado."));
+		
+	}
+	
+	public List<Funcionario> procurarTodos(){
+		List<Funcionario> listaFunc = repository.findAll();
+		return listaFunc;
+		
+	}
+	
+	public String criarFuncionario(Funcionario func) {
+		
+		if (func.getEmail() == null || func.getEmail().isBlank()) {
+			throw new DadosInvalidosException("Email é obrigatório.");
+		}
+		if(func.getNome() == null || func.getNome().isBlank()) {
+			throw new DadosInvalidosException("Nome é obrigatório.");
+		}
+		
+		repository.save(func);
+		return "Funcionário salvo no sistema";
+		
+	}
+	
+	public String editarDepartamento(Long id, Funcionario func) {
+		Funcionario response = procurarPorId(id);
+		
+		if(func.getEmail() != null) {
+			response.setEmail(func.getEmail());
+		}
+		
+		if(func.getNome() != null) {
+			response.setNome(func.getNome());
+		}
+		
+		repository.save(response);
+		return "Departamento atualizado com sucesso";
+	}
+	
+	public String deletarFuncionario(Long id) {
+		Funcionario response = procurarPorId(id);
+		repository.delete(response);
+		return "Funcionario deletado.";
+	}
+	
+	
+}
